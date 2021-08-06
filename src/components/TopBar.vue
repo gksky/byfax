703d2ba8f6fc8b0367183b3d69dbf5947366ce45
Alexander Gutkovsky <a.gutkovsky@gmail.com>
2021-08-06 16:03:04 +0300

    add: topbar

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="topBar">
     <div class="auth">
-      <button class="login">Вход</button>
-      <button class="register">Регистрация</button>
-      <select class="lang">
-        <option>ENG</option>
-        <option>РУС</option>
-        <option>БЕЛ</option>
-      </select>
+      <div class="social">
+        fdsgdg
+      </div>
+      <div>
+        <button class="login">Вход</button>
+        <button class="register">Регистрация</button>
+        <select class="lang">
+          <option>ENG</option>
+          <option>РУС</option>
+          <option>БЕЛ</option>
+        </select>
+      </div>
     </div>
   </div>
 </template>
@@ -21,16 +26,37 @@ export default {
 
 <style>
 .topBar {
-  
+  height: 50px;
   background-color: #012c3d;
+  
+}
+.social {
+  display: none;
 }
 .auth {
   margin: 0 auto;
   max-width: 1100px;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: center;
+}
+button {
+  font-size: 14px;
+  min-height: 30px;
+  text-transform: uppercase;
+  padding: 5px 8px;
+  -webkit-border-radius: 4px;
+  -moz-border-radius: 4px;
+  border-width: 0;
+  border-radius: 4px;
+  text-align: center;
 }
 button.login {
   background-color: #769201;
   color: #ffffff;
+  
 }
 button.register {
   background-color: #005e78;
